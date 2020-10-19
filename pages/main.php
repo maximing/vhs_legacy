7ddeeb1d58b3bf84d078c3ae6b7c737c1bfac2c5
Maxim Ignatov <error25@mail.ru>
2020-10-19 19:05:48 +0600

    contest

--- pages/main.php
+++ pages/main.php
@@ -2,6 +2,8 @@
 <div class="entry-content">
     <p style="text-align: center;">Оцифровка видеокассет VHS.</p>
 
+    <p style="text-align: center; color: red"><strong>АКЦИЯ 5+1!</strong> При оцифровке 5 кассет шестая бесплатно!</p>
+
     <a href="/static/img/main_page_big_image.jpg">
         <img class="size-full wp-image-65" style="margin: 4px 0 10px 10px;"
              title="vhs_otsifrovka_videokasset_digital_types"
