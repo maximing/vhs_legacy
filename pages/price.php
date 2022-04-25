064ab96ef59af390876fb25a5cba9b3456f87d2f
Maxim Ignatov <error25@mail.ru>
2022-04-25 15:54:06 +0600

    fix price

--- pages/price.php
+++ pages/price.php
@@ -1,4 +1,4 @@
 <h2 class="entry-title">Цены</h2>
 <div class="entry-content">
-    <p><span style="color: orangered">АКЦИЯ!</span> Стоимость оцифровки одной VHS видеокассеты - <strong>1 500</strong> тенге</p>
+    <p><span style="color: orangered">АКЦИЯ!</span> Стоимость оцифровки одной VHS видеокассеты <br> <strong>1 500</strong> тенге</p>
 </div>
